fc88eadf45319dfd9c121746100155be52779ffc
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-10-07 10:36:49 +0200

    SqlClient NuGet installed BookingRepo.GetAll() - not testet

--- VaskEnTid-Library/Repo/BookingRepo.cs
+++ VaskEnTid-Library/Repo/BookingRepo.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using VaskEnTidLibrary.Model;
 
 namespace VaskEnTid_Library.Repo
 {
     internal class BookingRepo
     {
+        static string connectionString = "mangler";
+
+        public List<Booking> GetAll()
+        {
+            List<Booking> bookings = new List<Booking>();
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string sqlCode = "SELECT BookingID, MachineID, PhoneNumber, Date, Time FROM Booking; ";
+
+                SqlCommand command = new SqlCommand(sqlCode, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Booking booking = new Booking(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), DateOnly.FromDateTime(reader.GetDateTime(3)), TimeOnly.FromDateTime(reader.GetDateTime(4)));
+
+                    bookings.Add(booking);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error:\n" + ex);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return bookings;
+        }
     }
 }
