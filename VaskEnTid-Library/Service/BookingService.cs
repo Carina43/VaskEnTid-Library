94bc144e9e125df484af0d3e324a097d674a9023
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-10-08 15:31:22 +0200

    Service klasse oprettet

---
+++ VaskEnTid-Library/Service/BookingService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaskEnTidLibrary.Model;
+using VaskEnTid_Library.Repo;
+
+namespace VaskEnTid_Library.Service
+{
+    public class BookingService
+    {
+        private IDatabaseRepo<Booking, int> _bookingRepo = new BookingRepo();
+
+        public List<Booking> GetAllBookings()
+        {
+            List<Booking> bookings = _bookingRepo.GetAll();
+
+            return bookings;
+        }
+    }
+}
